d5906f16325a5d5ae81894b5066d714f238bf20f
dmondo <dannysmondo@gmail.com>
2020-06-06 01:20:55 -0700

    feat(store): state management using react context

---
+++ src/store/Store.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const initialState: IState = {
+  user: '',
+};
+
+export const Store = React.createContext<IState | any>(initialState);
+
+const reducer = (state: IState, action: IAction): IState => {
+  switch (action.type) {
+    case 'USER':
+      return { ...state, user: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const StoreProvider = (props: any): JSX.Element => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const { children } = props;
+  return (
+    <Store.Provider
+      value={{ state, dispatch }}
+    >
+      {children}
+    </Store.Provider>
+  );
+};

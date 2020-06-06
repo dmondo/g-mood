e3b3e831cce776e6b622eb5cf69e7031bc519b68
dmondo <dannysmondo@gmail.com>
2020-06-06 15:40:20 -0700

    refactor(index.tsx): wrap app in store provider for context

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { StoreProvider } from './store/Store';
 
 ReactDOM.render(
-  <App />,
+  <StoreProvider>
+    <App />
+  </StoreProvider>,
   document.getElementById('sudokuJS'),
 );

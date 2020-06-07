c2d1bea589484e637ab92bc58b25e1412add2ed1
dmondo <dannysmondo@gmail.com>
2020-06-07 14:14:57 -0700

    feat(Square): conditional css class when selected

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -5,26 +5,31 @@ import { Store } from '../store/Store';
 
 const Square = ({ val, row, col }): JSX.Element => {
   const { state, dispatch } = React.useContext(Store);
-  const { currentPuzzle } = state;
+  const { currentPuzzle, inputSquare } = state;
+
+  const selected = inputSquare[0] === row && inputSquare[1] === col;
+  const inputState = selected ? 'square selected' : 'square';
 
   const enterVal = (): void => {
-    const numHandler = (e) => {
+    const numHandler = (e: KeyboardEvent) => {
       const pressed = String.fromCharCode(e.keyCode);
 
       if (!Number.isNaN(Number(pressed))) {
         currentPuzzle[row][col] = pressed;
         dispatch({ type: 'PUZZLE', payload: currentPuzzle });
+        dispatch({ type: 'INPUT', payload: [] });
         document.removeEventListener('keydown', numHandler);
       }
     };
 
+    dispatch({ type: 'INPUT', payload: [row, col] });
     document.addEventListener('keydown', numHandler);
   };
 
   return (
     <>
       <button
-        className="square"
+        className={inputState}
         type="button"
         onClick={() => enterVal()}
       >

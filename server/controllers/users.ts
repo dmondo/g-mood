8bb0eb8a2d434f39503d0e0331eef1d1427a983d
dmondo <dannysmondo@gmail.com>
2020-06-08 15:14:10 -0700

    refactor(controller): auth route for users

--- server/controllers/users.ts
+++ server/controllers/users.ts
@@ -2,11 +2,7 @@ import { Request, Response } from 'express';
 import { findUser, saveUser } from '../database/index';
 
 const postUser = (req: Request, res: Response): void => {
-  console.log('req.body', req.body);
   const { username, email, password } = req.body;
-  console.log('username', username);
-  console.log('email', email);
-  console.log('password', password);
   if (!username || !email || !password) {
     res.status(400).json({ msg: 'please enter all fields' });
     return;
@@ -16,23 +12,29 @@ const postUser = (req: Request, res: Response): void => {
     if (err) {
       res.sendStatus(500);
     } else if (type === 'exists') {
-      console.log('type = exists');
       res.status(400).json({ msg: 'user already exists' });
     } else {
-      console.log('type doesnt exist');
       res.sendStatus(200);
     }
   });
 };
 
-const getUser = (req: Request, res: Response): void => {
-  findUser(req.body, (err: Error, data: IUser[]) => {
+const postAuth = (req: Request, res: Response): void => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({ msg: 'please enter all fields' });
+    return;
+  }
+
+  findUser(req.body, (err: Error, data: IUserName, type: string) => {
     if (err) {
       res.sendStatus(500);
+    } else if (type === 'badUser') {
+      res.status(400).json({ mgs: 'incorrect login info' });
     } else {
       res.json(data);
     }
   });
 };
 
-export { postUser, getUser };
+export { postUser, postAuth };

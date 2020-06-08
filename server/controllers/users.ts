b59e431ffb9f7783c012583d29eddbef391dc9c2
dmondo <dannysmondo@gmail.com>
2020-06-08 13:46:03 -0700

    feat(controller): user controller does basic input validation and passes to db CRUD

--- server/controllers/users.ts
+++ server/controllers/users.ts
@@ -2,10 +2,24 @@ import { Request, Response } from 'express';
 import { findUser, saveUser } from '../database/index';
 
 const postUser = (req: Request, res: Response): void => {
-  saveUser(req.body, (err: Error) => {
+  console.log('req.body', req.body);
+  const { username, email, password } = req.body;
+  console.log('username', username);
+  console.log('email', email);
+  console.log('password', password);
+  if (!username || !email || !password) {
+    res.status(400).json({ msg: 'please enter all fields' });
+    return;
+  }
+
+  saveUser(req.body, (err: Error, type?: string) => {
     if (err) {
       res.sendStatus(500);
+    } else if (type === 'exists') {
+      console.log('type = exists');
+      res.status(400).json({ msg: 'user already exists' });
     } else {
+      console.log('type doesnt exist');
       res.sendStatus(200);
     }
   });

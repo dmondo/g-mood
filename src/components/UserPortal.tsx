f938f2507177e33f3dd3f889ffa478007fa85afa
dmondo <dannysmondo@gmail.com>
2020-06-08 13:45:38 -0700

    feat(userPortal): userPortal renders once logged in

---
+++ src/components/UserPortal.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Board from './Board';
+import '../style/userPortal.css';
+import { Store } from '../store/Store';
+import { solveBoard } from '../../lib/utils';
+
+const UserPortal = (): JSX.Element => {
+  const { state, dispatch } = React.useContext(Store);
+  useEffect(() => {
+    (async () => {
+      const data = await fetch('/puzzles');
+      const puzzles = await data.json();
+      const inProgress = puzzles[0].puzzle.map((row: (string|number)[]) => row.slice());
+      const emptySolution = puzzles[0].puzzle.map((row: (string|number)[]) => row.slice());
+      dispatch({ type: 'PUZZLE', payload: inProgress });
+      dispatch({ type: 'START', payload: emptySolution });
+    })();
+  }, []);
+
+  const getSolved = () => {
+    const { startingPuzzle } = state;
+    const solvedPuzzle = solveBoard(startingPuzzle);
+    dispatch({ type: 'SOLUTION', payload: solvedPuzzle });
+  };
+
+  const removeSolved = () => {
+    dispatch({ type: 'SOLUTION', payload: false });
+  };
+
+  return (
+    <>
+      {/* <div className="title">
+        sudokuJS
+      </div> */}
+      <Grid item xs={12}>
+        <Grid container justify="flex-start" spacing={2}>
+          <Grid item sm={6}>
+            <Board solved={false} />
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={getSolved}
+            >
+              solve
+            </Button>
+          </Grid>
+          <Grid item sm={6}>
+            {state.solution && <Board solved />}
+            {state.solution && (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={removeSolved}
+              >
+                hide
+              </Button>
+            )}
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default UserPortal;

e28165df71fbf7aad1d6fd2e9afb71dbf3e6d5d7
dmondo <dannysmondo@gmail.com>
2020-06-08 13:47:19 -0700

    refactor(app): app conditionally renders login page or landing page

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,66 +1,31 @@
-import React, { useEffect } from 'react';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import Board from './Board';
+import React from 'react';
 import '../style/App.css';
 import { Store } from '../store/Store';
-import { solveBoard } from '../../lib/utils';
+import UserPortal from './UserPortal';
+import Login from './Login';
 
 const App = (): JSX.Element => {
-  const { state, dispatch } = React.useContext(Store);
-
-  useEffect(() => {
-    (async () => {
-      const data = await fetch('/puzzles');
-      const puzzles = await data.json();
-      const inProgress = puzzles[0].puzzle.map((row: (string|number)[]) => row.slice());
-      const emptySolution = puzzles[0].puzzle.map((row: (string|number)[]) => row.slice());
-      dispatch({ type: 'PUZZLE', payload: inProgress });
-      dispatch({ type: 'START', payload: emptySolution });
-    })();
-  }, []);
-
-  const getSolved = () => {
-    const { startingPuzzle } = state;
-    const solvedPuzzle = solveBoard(startingPuzzle);
-    dispatch({ type: 'SOLUTION', payload: solvedPuzzle });
-  };
-
-  const removeSolved = () => {
-    dispatch({ type: 'SOLUTION', payload: false });
-  };
+  const { state } = React.useContext(Store);
+  const { loginStatus, user } = state;
 
   return (
     <>
-      <div className="title">
-        sudokuJS
-      </div>
-      <Grid item xs={12}>
-        <Grid container justify="flex-start" spacing={2}>
-          <Grid item sm={6}>
-            <Board solved={false} />
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={getSolved}
-            >
-              solve
-            </Button>
-          </Grid>
-          <Grid item sm={6}>
-            {state.solution && <Board solved />}
-            {state.solution && (
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={removeSolved}
-              >
-                hide
-              </Button>
-            )}
-          </Grid>
-        </Grid>
-      </Grid>
+      {
+        !loginStatus && (
+          <div className="title">
+            sudokuJS
+          </div>
+        )
+      }
+      {
+        loginStatus && (
+          <div className="title">
+            {`sudokuJS -- welcome ${user}`}
+          </div>
+        )
+      }
+      {!loginStatus && <Login />}
+      {loginStatus && <UserPortal />}
     </>
   );
 };

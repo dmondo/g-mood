0d6c3327de0de4f171f95c0b22aef030b2ee13bd
dmondo <dannysmondo@gmail.com>
2020-06-07 23:11:05 -0700

    feat(router): REST api routes for users

--- server/router.ts
+++ server/router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import path from 'path';
 import { getPuzzle, getPuzzles, postPuzzle } from './controllers/puzzles';
+import { getUser, postUser } from './controllers/users';
 
 const router = Router();
 
@@ -13,5 +14,9 @@ router.get('/puzzles', getPuzzles);
 router.get('/puzzles/:uuid', getPuzzle);
 
 router.post('/puzzles', postPuzzle);
+
+router.get('/users', getUser);
+
+router.post('/users', postUser);
 
 export default router;

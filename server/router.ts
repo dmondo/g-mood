03311906c3326500a67542f48932f46fce2718c8
dmondo <dannysmondo@gmail.com>
2020-06-07 22:27:32 -0700

    feat(router) REST api routes for puzzle database

--- server/router.ts
+++ server/router.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import path from 'path';
+import { getPuzzle, getPuzzles, postPuzzle } from './controllers/puzzles';
 
 const router = Router();
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'src', 'index.html'));
 });
+
+router.get('/puzzles', getPuzzles);
+
+router.get('/puzzles/:uuid', getPuzzle);
+
+router.post('/puzzles', postPuzzle);
 
 export default router;

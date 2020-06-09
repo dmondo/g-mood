c2d8794b8600803a99c011d13da90feadff0d5e4
dmondo <dannysmondo@gmail.com>
2020-06-08 20:08:58 -0700

    feat(app): if session token exists in storage, login

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,8 +5,18 @@ import UserPortal from './UserPortal';
 import Login from './Login';
 
 const App = (): JSX.Element => {
-  const { state } = React.useContext(Store);
+  const { state, dispatch } = React.useContext(Store);
   const { loginStatus, user } = state;
+
+  React.useEffect(() => {
+    const token = window.localStorage.getItem('sudokuJS');
+    if (token) {
+      const username = window.localStorage.getItem('sudokuName');
+      dispatch({ type: 'USER', payload: username });
+      dispatch({ type: 'TOKEN', payload: token });
+      dispatch({ type: 'LOGIN', payload: true });
+    }
+  }, []);
 
   return (
     <>
